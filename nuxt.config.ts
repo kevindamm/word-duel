294965f0d818c1c1df2bd83c83f3b8fbeaf7793f
Kevin Damm <github@kevindamm.com>
2025-08-28 23:31:09 -0500

    Nuxt boilerplate and the first version of a favicon

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: "cloudflare_module",
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  },
+
+  app: {
+    head: {
+      link: [
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" }
+      ]
+    }
+  },
+
+  modules: ["nitro-cloudflare-dev"]
+})
